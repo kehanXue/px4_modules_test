e957d4ad4d2f42fc652ec9af9626e28297b69056
Kehan.Xue <kehan.xue@gmail.com>
2019-07-24 15:32:00 +0800

    Fixed tf's velocity transfrom bug.

--- src/px4_modules_test_node.cpp
+++ src/px4_modules_test_node.cpp
@@ -37,14 +37,6 @@ int main(int argc, char** argv)
     // The setpoint publishing rate MUST be faster than 2Hz
     ros::Rate rate(20);
 
-    tf::TransformListener odom_base_tf_listener;
-    geometry_msgs::Vector3Stamped linear_body_vel{};
-    linear_body_vel.header.stamp = ros::Time::now();
-    linear_body_vel.header.frame_id = "camera_link";
-    linear_body_vel.vector.x = 1.;
-    linear_body_vel.vector.y = 0.;
-    linear_body_vel.vector.z = 0.;
-
     // geometry_msgs::PoseStamped temp_pose;
     // temp_pose.pose.position.x = 0;
     // temp_pose.pose.position.y = 0;
@@ -57,6 +49,7 @@ int main(int argc, char** argv)
     //     rate.sleep();
     // }
 
+    tf::TransformListener odom_base_tf_listener(ros::Duration(2.));
 
     vwpp::PX4Interface::getInstance()->switchOffboard();
     vwpp::PX4Interface::getInstance()->unlockVehicle();
@@ -71,15 +64,27 @@ int main(int argc, char** argv)
     pid_controller_x.setTarget(0.);
     pid_controller_y.setTarget(0.);
     pid_controller_z.setTarget(1.);
-    // pid_controller_yaw.setTarget(vwpp::PX4Interface::getInstance()->getCurYaw());
-    pid_controller_yaw.setTarget(vwpp::PX4Interface::getInstance()->getCurYaw() + M_PI);
-    ROS_INFO("Target yaw: %lf", vwpp::PX4Interface::getInstance()->getCurYaw());
+    pid_controller_yaw.setTarget(vwpp::PX4Interface::getInstance()->getCurYaw());
+    // pid_controller_yaw.setTarget(vwpp::PX4Interface::getInstance()->getCurYaw() + M_PI);
+    // ROS_INFO("Target yaw: %lf", vwpp::PX4Interface::getInstance()->getCurYaw());
 
     // TODO rad
 
+    int64_t cnt = 0;
     while (ros::ok())
     {
+
         vwpp::PX4Interface::getInstance()->update();
+
+        if (fabs(vwpp::PX4Interface::getInstance()->getCurZ() - pid_controller_z.getTarget()) <= 0.10)
+        {
+            cnt++;
+            if (cnt >= 15)
+            {
+                pid_controller_x.setTarget(3.0);
+                cnt = 0;
+            }
+        }
 
         pid_controller_x.update(vwpp::PX4Interface::getInstance()->getCurX());
         pid_controller_y.update(vwpp::PX4Interface::getInstance()->getCurY());
@@ -92,28 +97,38 @@ int main(int argc, char** argv)
         cmd_vel.linear.x = pid_controller_x.output();
         cmd_vel.linear.y = pid_controller_y.output();
         cmd_vel.linear.z = pid_controller_z.output();
-        // ROS_INFO("Cmd_vel X: %lf, Cmd_vel Y: %lf, Cmd_vel Z: %lf", cmd_vel.linear.x, cmd_vel.linear.y,
-        //          cmd_vel.linear.z);
+        ROS_INFO("Cmd_vel X: %lf, Cmd_vel Y: %lf, Cmd_vel Z: %lf", cmd_vel.linear.x, cmd_vel.linear.y,
+                 cmd_vel.linear.z);
         cmd_vel.angular.x = 0;
         cmd_vel.angular.y = 0;
         cmd_vel.angular.z = pid_controller_yaw.output();
-        // ROS_INFO("Yaw velocity output: %lf", cmd_vel.angular.z);
-        // vwpp::PX4Interface::getInstance()->publishLocalVel(cmd_vel);
+        ROS_INFO("Yaw velocity output: %lf", cmd_vel.angular.z);
+        vwpp::PX4Interface::getInstance()->publishLocalVel(cmd_vel);
 
+
+        geometry_msgs::Vector3Stamped linear_body_vel{};
+        linear_body_vel.header.stamp = ros::Time::now();
+        linear_body_vel.header.frame_id = "camera_link";
+        linear_body_vel.vector.x = 1.;
+        linear_body_vel.vector.y = 0.;
+        linear_body_vel.vector.z = 0.;
 
         // Test tf vector transformer
         geometry_msgs::Vector3Stamped linear_local_vel{};
         try
         {
+            // odom_base_tf_listener.transformVector("camera_odom_frame", ros::Time(1), linear_body_vel,
+            //                                       "camera_odom_frame", linear_local_vel);
+            //
             odom_base_tf_listener.transformVector("camera_odom_frame", linear_body_vel, linear_local_vel);
         }
         catch (tf::TransformException &tf_ex)
         {
-            ROS_ERROR("%s", tf_ex.what());
-            ros::Duration(1.0).sleep();
+            // ROS_ERROR("%s", tf_ex.what());
+            ros::Duration(0.2).sleep();
         }
-        ROS_INFO("Transformed X: %lf\nTransformed Y: %lf\nTransformed Z: %lf\n", linear_local_vel.vector.x,
-                 linear_local_vel.vector.y, linear_local_vel.vector.z);
+        // ROS_INFO("Transformed X: %lf\nTransformed Y: %lf\nTransformed Z: %lf\n", linear_local_vel.vector.x,
+        //          linear_local_vel.vector.y, linear_local_vel.vector.z);
 
         rate.sleep();
     }
